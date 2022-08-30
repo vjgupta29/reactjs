ded64f3b83966b0fe38a520734e8d0c205864f89
Vijay Gupta <vjgupta29@users.noreply.github.com>
2022-08-30 16:17:10 +0530

    Add files via upload

---
+++ src/myComponents/TodoItems.js
@@ -0,0 +1,78 @@
+import React from "react";
+
+export default function TodoItems({ todo, onDelete , onInject}) {
+  return (
+    <div className="accordion" id="accordionExample">
+      <div className="accordion-item">
+        <h2 className="accordion-header" id="headingOne">
+          <button
+            className="accordion-button"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#collapseOne"
+            aria-expanded="true"
+            aria-controls="collapseOne"
+          >
+            TradeId: {todo.title}
+          </button>
+        </h2>
+        <div
+          id="collapseOne"
+          className="accordion-collapse collapse show"
+          aria-labelledby="headingOne"
+          data-bs-parent="#accordionExample"
+        >
+          <div className="accordion-body">
+            <div className="mb-3">
+              <label htmlFor="exampleFormControlTextarea1" className="form-label">
+                Trade Message
+              </label>
+              <textarea
+                className="form-control"
+                id="exampleFormControlTextarea1"
+                rows="3"
+                defaultValue={todo.desc}
+              ></textarea>
+            </div>
+          </div>
+          <div>
+            <button
+              className="btn btn-danger"
+              onClick={() => {
+                onDelete(todo);
+              }}
+            >
+              {" "}
+              Delete{" "}
+            </button>
+            {"    "}
+            <button
+              className="btn btn-success"
+              onClick={() => {
+                onInject(todo);
+              }}
+            >
+              {" "}
+              Inject{" "}
+            </button>
+            {"    "}
+            <div className="form-check form-check-inline">
+                <input className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1" disabled/>
+                <label className="form-check-label" htmlFor="inlineRadio1">DEV (disabled)</label>
+            </div>
+            <div className="form-check form-check-inline">
+              <input className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2" disabled/>
+              <label className="form-check-label" htmlFor="inlineRadio2">QA (disabled)</label>
+            </div>
+            <div className="form-check form-check-inline">
+              <input className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio3" value="option3" />
+              <label className="form-check-label" htmlFor="inlineRadio3">UAT</label>
+            </div>
+              </div>
+              <div className="blank"><br></br><hr></hr></div>
+            </div>
+            :<div></div>}
+      </div>
+    </div>
+  );
+}
